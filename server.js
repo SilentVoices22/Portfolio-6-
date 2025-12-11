9d0e09501b22305a89173be2fd84d5393f3aacd8
victorbischoff <morkebergv@gmail.com>
2025-12-11 10:34:41 +0100

    Added /app/applicants/it-applicants endpoint

--- server.js
+++ server.js
@@ -15,6 +15,18 @@ const collectionName = 'applicants';
 let db;
 let collection;
 
+
+const itUddannelser = ["PB i Softwareudvikling", "PB i IT-arkitektur", "Økonomi og it", "Datamatiker", "Digital konceptudvikling", "IT-teknolog", "PB i IT-sikkerhed", "PB i Webudvikling"];
+
+app.get('/app/applicants/it-applicants', async (req, res) => {
+  const results = await collection.find(
+    { INSTITUTIONSAKT_BETEGNELSE: { $in: itUddannelser } },
+    { projection: { Køn: 1, INSTITUTIONSAKT_BETEGNELSE: 1, _id: 0 } }
+  ).toArray();
+  
+  res.json(results);
+});
+
 async function connectToMongo() {
   try {
     const client = new MongoClient(url);

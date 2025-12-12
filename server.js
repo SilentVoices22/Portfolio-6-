0bfc93ec40a6741791ab08d6dba62c6839570b22
victorbischoff <morkebergv@gmail.com>
2025-12-12 11:51:28 +0100

    moved code

--- server.js
+++ server.js
@@ -16,17 +16,6 @@ let db;
 let collection;
 
 
-const itUddannelser = ["PB i Softwareudvikling", "PB i IT-arkitektur", "Økonomi og it", "Datamatiker", "Digital konceptudvikling", "IT-teknolog", "PB i IT-sikkerhed", "PB i Webudvikling"];
-
-app.get('/app/applicants/it-applicants', async (req, res) => {
-  const results = await collection.find(
-    { INSTITUTIONSAKT_BETEGNELSE: { $in: itUddannelser } },
-    { projection: { Køn: 1, INSTITUTIONSAKT_BETEGNELSE: 1, _id: 0 } }
-  ).toArray();
-  
-  res.json(results);
-});
-
 async function connectToMongo() {
   try {
     const client = new MongoClient(url);
@@ -40,6 +29,18 @@ async function connectToMongo() {
     process.exit(1);
   }
 }
+
+const itUddannelser = ["PB i Softwareudvikling", "PB i IT-arkitektur", "Økonomi og it", "Datamatiker", "Digital konceptudvikling", "IT-teknolog", "PB i IT-sikkerhed", "PB i Webudvikling"];
+
+app.get('/app/applicants/it-applicants', async (req, res) => {
+  const results = await collection.find(
+    { INSTITUTIONSAKT_BETEGNELSE: { $in: itUddannelser } },
+    { projection: { Køn: 1, INSTITUTIONSAKT_BETEGNELSE: 1, _id: 0 } }
+  ).toArray();
+  
+  res.json(results);
+});
+
 
 app.get('/app/salary', async (req, res) => {
   const results = await db.collection(salary_db).find({}).toArray();

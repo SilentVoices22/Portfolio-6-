2016d1924a1e5b6a713f8caaaebb8b7e800f52ba
zrh116 <jaol0005@stud.ek.dk>
2025-12-12 11:44:26 +0100

    Update charts.js

--- charts.js
+++ charts.js
@@ -1,3 +1,28 @@
+async function fetchSalary(){
+    try{
+        const res = await fetchSalary("http://localhost:8080/app/salary");
+        const data = await res.json();
+        console.log("Averrage salary" , data)
+        return data;
+        } catch (error) {
+            console.error("Could not fetch", error);
+        }
+    }
+
+async function fetchGenderWork(){
+    try{
+        const res = await fetchSalary("http://localhost:8080/app/applicants/gender_work");
+        const data = await res.json();
+        console.log("Gender work" , data)
+        return data;
+        } catch (error) {
+            console.error("Could not fetch", error);
+        }
+    }
+
+
+
+
 const ctx = document.querySelector('#chart').getContext('2d');
 const chart = new Chart(ctx, {
     type: 'bar',
@@ -8,3 +33,4 @@ const chart = new Chart(ctx, {
         }]
     }
 });
+
